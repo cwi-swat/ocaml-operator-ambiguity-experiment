4d88d5b8752a760527f1b3d0af431e60344a1a8c
Ali Afroozeh <afroozeh@gmail.com>
2013-08-11 11:50:42 +0200

    compare_all.rb added.

---
+++ compare_all.rb
@@ -0,0 +1,19 @@
+files = Array.new
+home = ""
+
+
+File.foreach('successful_parse_disambiguate.txt') do |line|
+  f = line.chomp
+  files << f
+end
+
+for i in 0..files.size  
+  puts "grep -v '^\\s*$' #{home}#{files[i]}.rascal > #{home}#{files[i]}.rascal.stripped"
+  puts "grep -v '^\\s*$' #{home}#{files[i]}.ocaml > #{home}#{files[i]}.ocaml.stripped"
+
+  `grep -v '^\\s*$' #{home}#{files[i]}.rascal > #{home}#{files[i]}.rascal.stripped`
+  `grep -v '^\\s*$' #{home}#{files[i]}.ocaml > #{home}#{files[i]}.ocaml.stripped`
+
+  puts "diff -w #{home}#{files[i]}.rascal.stripped #{home}#{files[i]}.ocaml.stripped"
+  `diff -w #{home}#{files[i]}.rascal.stripped #{home}#{files[i]}.ocaml.stripped > #{home}#{files[i]}.diff`
+end
